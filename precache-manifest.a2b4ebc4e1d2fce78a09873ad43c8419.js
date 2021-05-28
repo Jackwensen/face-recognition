7fd5115bbdb05b23136dcf5f258f660ac4767c1a
levan <744915067@qq.com>
2021-05-28 22:37:54 +0800

    Updates

---
+++ precache-manifest.a2b4ebc4e1d2fce78a09873ad43c8419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c56c3777fc02240163c9d97522d3c8",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "f7c8f397d5783f3a3de5",
+    "url": "/face-recognition/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "68e00d8977dbf9553c03",
+    "url": "/face-recognition/static/css/main.5329af53.chunk.css"
+  },
+  {
+    "revision": "f7c8f397d5783f3a3de5",
+    "url": "/face-recognition/static/js/2.d0ae8d5f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/face-recognition/static/js/2.d0ae8d5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e00d8977dbf9553c03",
+    "url": "/face-recognition/static/js/main.a9ab940c.chunk.js"
+  },
+  {
+    "revision": "2f6cdf56422103aed269",
+    "url": "/face-recognition/static/js/runtime-main.e9cffbfb.js"
+  }
+]);
